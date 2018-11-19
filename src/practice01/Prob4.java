de3d15e4c7dd63f5c453092782e0a51333e9fedb
RUN <RUN@169.254.137.113>
2018-11-19 23:52:54 +0900

    4 end

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -6,13 +6,24 @@ public class Prob4 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("문자열을 입력하세요:");
 		String s = scanner.nextLine();
-		
+
 		/* 코드를 완성합니다. */
-		System.out.println(s);
-		
+		int size = s.length();
+		for (int i = 0; i <= size; i++) {
+			int j = 0;
+			if (j < i) {
+				for (; j <= i - 1; j++) {
+					System.out.print(s.charAt(j));
+				}
+				System.out.println("");
+
+			}
+
+		}
+
 		scanner.close();
 	}
 
